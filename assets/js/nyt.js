33f383e0692b4a8abe8bd8b7ed23bd0537283c42
alex@netwaveinteractive.com <alex.rosenkranz@gmail.com>
2016-12-14 10:36:11 -0500

    playing with parameters and styling

--- assets/js/nyt.js
+++ assets/js/nyt.js
@@ -2,9 +2,10 @@ $(document).ready(function() {
 
   var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
   var query;
-  var numRecords;
+  var numRecords = 5;
   var beginDate;
   var endDate;
+  var parameters = [];
   
 
 
@@ -15,7 +16,7 @@ $('.submit').on('click',function(event) {
   event.preventDefault();
 
   
-  query = $('#searchTerm').val();
+  query = '&q=' + $('#searchTerm').val().trim();
   $('#searchTerm').val("");
   console.log(query);
   numRecords = $('#numRecords').val();
@@ -28,12 +29,14 @@ $('.submit').on('click',function(event) {
   $('#endDate').val("");
   console.log(endDate);
 
+  parameters.push(query)
+
 
   if ($('#startDate').val() == '' && $('#endDate').val() == ''){
     url += '?' + $.param({
       'api-key': "eae7407fee8d450589f3b327a198477a",
       'q': query,
-      
+      'sort': 'newest',
     });
     console.log(url);
   }
@@ -43,13 +46,15 @@ $('.submit').on('click',function(event) {
       'q': query,
       'begin_date': beginDate,
       'end_date': endDate,
+      'sort': 'newest',
     });
   }
   else if ($('#startDate').val() != ''){
     url += '?' + $.param({
       'api-key': "eae7407fee8d450589f3b327a198477a",
       'q': query,
-      'begin_date': beginDate,    
+      'begin_date': beginDate, 
+      'sort': 'newest',   
     });
   }
   else if ($('#endDate').val() != ''){
@@ -57,6 +62,7 @@ $('.submit').on('click',function(event) {
       'api-key': "eae7407fee8d450589f3b327a198477a",
       'q': query,
       'end_date': endDate,    
+      'sort': 'newest',
     });
   }
 
@@ -64,14 +70,25 @@ $('.submit').on('click',function(event) {
     url: url,
     method: 'GET',
   }).done(function(result) {
+
     console.log(result);
+
     var article = result.response.docs
+
     for (var i = 0; i < numRecords; i++) {
       var articleDiv = $('<div>').addClass('col-xs-12 col-sm-12 col-md-6 article');
       var articleTitle = '<h2>' + article[i].headline.main +'</h2>';
+      
       var articleSection = '<h4>' + article[i].section_name + '</h4>';
       var articleP = '<p>' + article[i].lead_paragraph + '</p>';
-      articleDiv.append(articleTitle).append(articleSection).append(articleP);
+      if (article[i].multimedia != '') {
+        var articleImg = '<img src="http://nyt.com/' + article[i].multimedia[0].url + '"/>';
+        articleDiv.append(articleTitle).append(articleSection).append(articleImg).append(articleP);
+      }
+      else {
+        articleDiv.append(articleTitle).append(articleSection).append(articleP);
+      }
+      
       
       $('#searchResults').append(articleDiv);
     }
